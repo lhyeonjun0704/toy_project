ff179e6992aba34dda204019b3f87fa5ae048144
Hyonjun Lee <hello.git.toqur7000@naver.com>
2021-05-14 23:47:38 +0900

    daily
    wt...

--- start/js/share.js
+++ start/js/share.js
@@ -1,34 +1,67 @@
-function kakaoShare(){
+const url = 'https://toyprojectmimic.netlify.app/'
+
+function setShare(){
+  var resultImg = document.querySelector('#resultImg');
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = '십이간지 연애유형 결과';
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = url + 'img/image-' + resultAlt + '.png';
+  const shareURL = url + 'page/result-' + resultAlt + '.html';
+
   Kakao.Link.sendDefault({
     objectType: 'feed',
     content: {
-      title: '디저트 사진',
-      description: '아메리카노, 빵, 케익',
-      imageUrl:
-        'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+      title: shareTitle,
+      description: shareDes,
+      imageUrl: shareImage,
       link: {
-        mobileWebUrl: 'https://developers.kakao.com',
-        androidExecParams: 'test',
+        mobileWebUrl: shareURL,
+        webUrl: shareURL,
       },
-    },
-    social: {
-      likeCount: 10,
-      commentCount: 20,
-      sharedCount: 30,
     },
     buttons: [
       {
-        title: '웹으로 이동',
+        title: '결과확인하기',
         link: {
-          mobileWebUrl: 'https://developers.kakao.com',
-        },
-      },
-      {
-        title: '앱으로 이동',
-        link: {
-          mobileWebUrl: 'https://developers.kakao.com',
+          mobileWebUrl: shareURL,
+          webUrl: shareURL,
         },
       },
     ]
   });
 }
+
+// function kakaoShare(){
+//   Kakao.Link.sendDefault({
+//     objectType: 'feed',
+//     content: {
+//       title: '디저트 사진',
+//       description: '아메리카노, 빵, 케익',
+//       imageUrl:
+//         'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+//       link: {
+//         mobileWebUrl: 'https://developers.kakao.com',
+//         androidExecParams: 'test',
+//       },
+//     },
+//     social: {
+//       likeCount: 10,
+//       commentCount: 20,
+//       sharedCount: 30,
+//     },
+//     buttons: [
+//       {
+//         title: '웹으로 이동',
+//         link: {
+//           mobileWebUrl: 'https://developers.kakao.com',
+//         },
+//       },
+//       {
+//         title: '앱으로 이동',
+//         link: {
+//           mobileWebUrl: 'https://developers.kakao.com',
+//         },
+//       },
+//     ]
+//   });
+// }
